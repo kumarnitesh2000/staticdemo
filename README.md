af1a2714608f27665463529954939d650d36510c
kumarnitesh2000 <kumarnitesh2000.nk@gmail.com>
2020-09-14 16:25:38 -1000

    init

--- README.md
+++
@@ -1,2 +0,0 @@
-# staticWeb
-static websites
---
+++ precache-manifest.b73476407228189eaae92d10f4eba017.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfce1eb94bdc1ccced54d81210541cfa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16380d992cc00398e64e",
+    "url": "/static/css/main.e4f87294.chunk.css"
+  },
+  {
+    "revision": "cd0ba825abcf4ceaaa94",
+    "url": "/static/js/2.fdfac698.chunk.js"
+  },
+  {
+    "revision": "5b8b49146ea7ff4abb4f22a9d28772fe",
+    "url": "/static/js/2.fdfac698.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16380d992cc00398e64e",
+    "url": "/static/js/main.a8e9a72b.chunk.js"
+  },
+  {
+    "revision": "87c14c9b2478cfb3522d",
+    "url": "/static/js/runtime-main.e8b920fb.js"
+  }
+]);
